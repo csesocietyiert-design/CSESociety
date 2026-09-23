2bf94ddb0368304604b34dcf29e440b920828e7d
csesocietyiert-design <csesociety.iert@gmail.com>
2026-09-23 16:11:07 +0530

    Redirect visitors to the public homepage

--- shuffal/app/page.tsx
+++ shuffal/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function Home() {
-  redirect('/login');
+  redirect(process.env.NEXT_PUBLIC_HOMEPAGE_URL ?? 'http://localhost:5173');
 }
